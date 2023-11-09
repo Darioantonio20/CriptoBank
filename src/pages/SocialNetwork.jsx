b58e3fa39e43cfa093c592e6f0454703576a501d
LuisRamirez2328 <221260@ids.upchiapas.edu.mx>
2023-11-09 12:47:28 -0600

    [ADD empresa]

--- src/pages/SocialNetwork.jsx
+++ src/pages/SocialNetwork.jsx
@@ -3,32 +3,51 @@ import NavBar from "../components/molecules/NavBar";
 import Footer from "../components/molecules/Footer";
 import Bussnissman from "../assets/img/bussnissman.png";
 import Edificio from "../assets/img/edificio-de-oficinas.png"
+import Reunion from "../assets/img/groups.svg";
+import Evento from "../assets/img/events.svg";
+import Guardado from "../assets/img/user-following.svg";
+import Meet from "../assets/img/reunion.png";
 import "../assets/styles/Card.css";
 
 const usuarios = [
-  { id: 1, nombre: "Usuario 1", cargo: "Desarrollador Web", ubicacion: "Ciudad1, País1",  },
+  { id: 1, nombre: "Luis Antonio Ramirez Nucamendi", dedicación:"Investments in the stock market"},
  
 ];
 
 const TarjetaUsuario = ({ usuario }) => (
   <div className="w-72 h-24 bg-blue-500 rounded-lg ">
-  <div className="max-w-md mx-auto bg-white rounded-md overflow-hidden shadow-md mb-8 ml-10">
+  <div className=" max-w-md mx-auto bg-white rounded-md overflow-hidden shadow-md mb-8 ml-30">
   </div>
     <img src={Bussnissman} alt={`Foto de ${usuario.nombre}`} className="w-/12 h-auto object-cover" />
   
-    <div className="p-4">
-  <h2 className="text-xl font-bold mb-2">{usuario.nombre}</h2>
-  <p className="text-gray-600">Cargo: {usuario.cargo}</p>
-  <p className="text-gray-600">Ubicación: {usuario.ubicacion}</p>
-  <p className="mt-4">{usuario.descripcion}</p>
+    <div className=" p-4">
+  <h2 className=" text-xl font-bold mb-2">{usuario.nombre}</h2>
+  <p className="text-xl font-bold mb-2">{usuario.dedicación}</p>
   <div class="border-t border-blue-800 w-full"></div>
+  <div className="flex flex-row mt-4">
+  <p className="font-medium"> groups </p>
+        <img src={Reunion} className="ml-28 w-10 "/>
+      </div>
+      <div className="flex flex-row mt-4">
+  <p className="font-medium"> events </p>
+        <img src={Evento} className="ml-28 w-10 "/>
+      </div>
+      <div className="flex flex-row mt-4">
+  <p className="font-medium"> Hashtags followed </p>
+        <img src={Guardado} className="ml-20  w-8 "/>
+      </div>
+
 </div>
   </div>
 );
+
+
+  
+
 const empresas = [
   
-  { id: 1, nombre: "Empresa 1", tipo: "Tecnologia", año: "2023", tamaño: "Mediana" },
-  { id: 2, nombre: "Empresa 2", tipo: "Servicios", año: "2022", tamaño: "Grande" },
+  { id: 1, nombre: "America movil", tipo: "Technology", año: "2023", tamaño: "Median", descripcion:"It is a Mexican multinational telecommunications company based in Mexico City." },
+  { id: 2, nombre: "Femsa", tipo: "Services", año: "2022", tamaño: "Big", descripcion:"We began operations in 1890 as a brewing company in Monterrey, Mexico." },
 ];
 const TarjetaEmpresa = ({ empresa })=>(
   <div class="flex min-h-screen items-center justify-center ml-10 my-40">
@@ -40,10 +59,11 @@ const TarjetaEmpresa = ({ empresa })=>(
       </svg>
       <img src={Edificio}></img>
     </figure>
-    <h2 class="mt-4 text-xl font-bold text-indigo-600 dark:text-indigo-400">{empresa.empresa}</h2>
+    <h2 class="mt-4 text-xl font-bold text-indigo-600 dark:text-indigo-400">{empresa.nombre}</h2>
     <p class="mb-4 text-gray-600 dark:text-gray-300">{empresa.tipo}</p>
     <p class="mb-4 text-gray-600 dark:text-gray-300">{empresa.año}</p>
     <p class="mb-4 text-gray-600 dark:text-gray-300">{empresa.tamaño}</p>
+    <p class="mb-4 text-gray-600 dark:text-gray-300">{empresa.descripcion}</p>
     <div class="flex items-center justify-center">
       <a href="#" class="rounded-full bg-green-600 px-4 py-2 text-white hover:bg-green-700 dark:bg-green-400 dark:hover:bg-green-500">Ver</a>
       <a href="#" class="ml-4 rounded-full bg-red-600 px-4 py-2 text-white hover:bg-red-400 dark:bg-red-700 dark:hover:bg-red-600">ELiminar</a>
@@ -72,8 +92,9 @@ function SocialNetwork() {
           <div key={index} className="flex justify-between w-full mb-96">
             {fila.map((usuario) => (
               <TarjetaUsuario key={usuario.id} usuario={usuario} />
-              
+             
             ))}
+       
                {filasDeEmpresas.map((fila, index) => (
           <div key={index} className="flex justify-start w-full ">
             {fila.map((empresa) => (
@@ -86,6 +107,7 @@ function SocialNetwork() {
           
        
         ))}
+        
       </div>
       <Footer className="mb-60" />
     </>
